27d7791c41f07b6d816f365074cc82010310b241
Nay <nkhaldi@student.42.fr>
2020-08-21 08:38:23 +0200

    solution periodic event c programming ok

--- c_basics/periodicEvent.c
+++ c_basics/periodicEvent.c
@@ -4,8 +4,23 @@
 
 #define LOG_TRIGGER_TIME (5)
 
-static const time_t startTime;
-static int first = 0;
+// EXAMPLE
+/*
+time_t my_time;
+struct tm * timeinfo; 
+time (&my_time);
+timeinfo = localtime (&my_time);
+CCLog("year->%d",timeinfo->tm_year+1900);
+CCLog("month->%d",timeinfo->tm_mon+1);
+CCLog("date->%d",timeinfo->tm_mday);
+CCLog("hour->%d",timeinfo->tm_hour);
+CCLog("minutes->%d",timeinfo->tm_min);
+CCLog("seconds->%d",timeinfo->tm_sec);
+*/
+
+static time_t startTime;
+static int first = 1;
+static int startTimeSec = 0;
 // time(&startTime);
 // struct tm *startInfo;
 // static const int startTotalSec = startInfo->tm_hour * 60 * 60;
@@ -17,32 +32,36 @@ void func(void)
 
 int main()
 {
-	time_t rawtime;
+	if (first == 1)
+	{
+		first = 0;
+		time(&startTime);
+		struct tm *start_timeinfo = localtime(&startTime);
+		// printf("Starter local time and date: %s", asctime(start_timeinfo));
+		// int start_hour = start_timeinfo->tm_hour;
+		// startTimeSec = start_hour * 60 * 60;
+		startTimeSec = start_timeinfo->tm_sec;
+		printf("Starter time in seconds : %d\n", startTimeSec);
+	}
 
+	time_t rawtime;
 	struct tm *timeinfo;
 	int currentTimeSec = 0;
 
-	if (first == 0)
-	{
-		first = 1;
-		struct tm *start_timeinfo = localtime(&startTime);
-		int start_hour = start_timeinfo->tm_hour;
-		int start_TotalSec = start_timeinfo->tm_hour * 60 * 60;
-	}
-
 	while (1)
 	{
+		// recuperation heure en cours
 		time(&rawtime);
-
 		timeinfo = localtime(&rawtime);
+		// printf("CURRENT local time and date: %s", asctime(timeinfo));
+		currentTimeSec = timeinfo->tm_sec;
 
-		currentTimeSec = timeinfo->tm_hour * 60 * 60;
-
-		timeinfo = localtime(&rawtime);
-
-		sleep(LOG_TRIGGER_TIME);
-		printf("Starter local time and date: %s", asctime(timeinfo));
-		printf("Current local time and date: %s", asctime(timeinfo));
+		if (!first)
+		{
+			sleep(LOG_TRIGGER_TIME);
+		}
+		printf("Starter time in seconds : %d\n", startTimeSec);
+		printf("Current time in seconds : %d\n", currentTimeSec);
 	}
 
 	return 0;
