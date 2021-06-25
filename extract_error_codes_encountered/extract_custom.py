61de8540f0a6ce2686f58209f17aa000a56d37ae
nkhaldi <nkhaldi@sepro-group.com>
2021-06-25 05:12:25 +0200

    ok pour set une ligne

--- extract_error_codes_encountered/extract_custom.py
+++ extract_error_codes_encountered/extract_custom.py
@@ -1,47 +1,76 @@
-import re
-import csv
+import re, csv, os
 from bs4 import BeautifulSoup
 
 		# os.walk
 		# os.readir
 
-with open("50_111.html") as fp:
-	soup = BeautifulSoup(fp, "lxml")
-	print(soup.title.string)
+def scrapper():
+	with open("50_268.html") as fp:
+		soup = BeautifulSoup(fp, "lxml")
+
+		error_code = soup.title.string
+		list_content = soup.find_all(class_='pStandard')
+		list_header= soup.find_all(class_='pUeberschrift3')
+		list_param = soup.find_all(class_='pTabelle_Standard')
+
+		param_count = len(soup.find_all('tr'))
+
+		return list_content, list_header, list_param, param_count, error_code 
+
+def csvHeaderFiller(list_header, list_len):
+
+	header = []
 	
-	list_pStandard = soup.find_all(class_='pStandard')
-	pStd_len = len(list_pStandard)
+	header.append('msg_id')
+	header.append('msg_object')
 
-	list_pUeberschrift3= soup.find_all(class_='pUeberschrift3')
-	p3_len = len(list_pUeberschrift3)
-
-	list_pTabelle = soup.find_all(class_='pTabelle_Standard')
-	pTabelle_len = len(list_pTabelle)
-
-with open("msg_scrap.csv", "w") as output_file:
-
-	output_file.write(soup.find_all(class_='pCreationDate')[0].get_text() + '\n')
-	output_file.write(soup.title.string + ';')
-
-	for i in range(pStd_len):
-
-		if (list_pUeberschrift3[i].get_text() != "Parameter:"):
-			# print(list_pUeberschrift3[i].get_text())
-			output_file.write(list_pUeberschrift3[i].get_text() + '\n')
+	for i in range(list_len):
+		if (list_header[i].get_text() != "Parameter:"):
+			header.append(list_header[i].get_text()[0:-1])
 		else:
-			# print(list_pUeberschrift3[i].get_text())
-			output_file.write(list_pUeberschrift3[i].get_text() + '\n')
+			endColumn = list_header[i].get_text()[0:-1]
 
-			for param in list_pTabelle:
-				# print(param.get_text())
-				output_file.write(param.get_text() + ';')
-		# print(list_pStandard[i].get_text())
-		output_file.write(list_pStandard[i].get_text() + ';')
+	header.append(endColumn)
 
-	# print(soup.find_all(class_='pStandard')[0].get_text())
-	
-"""
-with open("messages.csv", "w") as output_file:
-	for data in result:
-		output_file.write(f"{data.decode()}\n"
-"""
+	return header
+
+def csvParamFiller(list_param, header_len):
+
+	row = []
+
+	param_len = len(list_param)
+
+	for i in range(header_len + 1):
+		row.append('')
+
+	for i in range(param_len - 1):
+		if (i % 2 == 0):
+			row.append(list_param[i].get_text() + list_param[i+1].get_text())
+	return row
+
+def csvFiller():
+
+	if os.path.exists("msg_scrap.csv"):
+		os.remove("msg_scrap.csv")
+
+	with open("msg_scrap.csv", "a") as output_file:
+
+		writer = csv.writer(output_file, delimiter=";")
+		list_content, list_header, list_param, param_count, error_code = scrapper()
+		header_len = len(list_header)
+
+		header = csvHeaderFiller(list_header, header_len)
+		writer.writerow(header)
+
+		row = csvParamFiller(list_param, header_len)
+
+		j = 0
+		row[j] = error_code
+
+		for i in range(len(list_content)):
+			j = i + 1
+			row[j] = list_content[i].get_text()
+
+		writer.writerow(row)
+
+csvFiller()
